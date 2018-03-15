f05e32c568ae40fc612b8497e2636491934e74d0
anton_nazarov1 <1102MortyMerr97>
2018-03-15 14:16:55 +0300

    Added ci

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -13,7 +13,7 @@ class CurrencyConverterTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        XCTAssertEqual(1, 2)
     }
     
     override func tearDown() {
